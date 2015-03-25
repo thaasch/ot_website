ba4b1a831acc35428dd0de4a732eca400943c995
Timo Haasch <timo_haasch@gmx.de>
2015-03-25 10:48:13 +0100

    initial

---
+++ Resources/Public/Js/Main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Timo on 24.03.15.
+ */
+jQuery(document).ready(function () {
+	$('[data-toggle="offcanvas"]').click(function () {
+		$('.row-offcanvas').toggleClass('active')
+	});
+});
